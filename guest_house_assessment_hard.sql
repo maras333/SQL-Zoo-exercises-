f4056ce5d3150df9a050492ab4188074d4b38a36
maras333 <maras.czyz@gmail.com>
2019-01-02 18:15:45 +0100

    15. guesthouse

--- guest_house_assessment_hard.sql
+++ guest_house_assessment_hard.sql
@@ -19,3 +19,34 @@ WHERE b2.room_no NOT IN (
   SELECT DISTINCT b1.room_no FROM booking b1
   WHERE b1.booking_date <= '2016-11-25' AND DATE_ADD(b1.booking_date, INTERVAL b1.nights DAY) > '2016-11-25'
 )
+
+-- 14. Single room for three nights required. A customer wants a single room for
+-- three consecutive nights. Find the first available date in December 2016.
+SELECT b1.room_no, b1.booking_date, DATE_ADD(b1.booking_date, INTERVAL b1.nights DAY) as booking_date_end
+FROM booking b1
+JOIN room r1 ON b1.room_no = r1.id
+JOIN (SELECT b1.room_no, b1.booking_date, DATE_ADD(b1.booking_date, INTERVAL b1.nights DAY) as booking_date_end FROM booking b1
+  JOIN room r1 ON b1.room_no = r1.id WHERE r1.room_type = 'single')
+  as temp_b_r 0N temp_b_r.room_no = b1.room_no AND temp_b_r.booking_date
+WHERE r1.room_type = 'single'
+
+
+-- 15. Gross income by week. Money is collected from guests when they leave. For each Thursday in November and December 2016,
+-- show the total amount of money collected from the previous Friday to that day, inclusive.
+SELECT DATE_ADD(MAKEDATE(2016, 7), INTERVAL WEEK(DATE_ADD(b1.booking_date, INTERVAL b1.nights - 5 DAY), 0) WEEK) AS i,
+SUM(rate1.amount*b1.nights) + SUM(e.amount) AS Total
+FROM booking b1
+JOIN room r1 ON b1.room_no = r1.id
+LEFT JOIN rate rate1 ON r1.room_type = rate1.room_type AND b1.occupants = rate1.occupancy
+LEFT JOIN (
+    SELECT
+      booking_id,
+      SUM(amount) as amount
+    FROM
+      extra
+    group by
+      booking_id
+  )
+  AS e
+  ON (e.booking_id = b1.booking_id)
+GROUP BY i
